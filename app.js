e6f3d08187280569d40bc74a7bd08f06efc22355
Mike Solomon <msolomon@thrillist.com>
2018-06-18 09:51:51 -0400

    use prod endpoint

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ var app = new Vue({
   methods: {
     loadLeaderboard: function() {
       console.log('fetching data')
-      //fetch('https://cors-anywhere.herokuapp.com/https://www.heytaco.chat/api/v1/json/leaderboard/T376NB673?days=3')
-      fetch('https://heytacoleaderboard.firebaseio.com/.json')
+      fetch('https://cors-anywhere.herokuapp.com/https://www.heytaco.chat/api/v1/json/leaderboard/T376NB673?days=1')
+      //fetch('https://heytacoleaderboard.firebaseio.com/.json')
       .then(response => response.json())
       .then(data => {
         //sort the leaderboard by count when in dev
-        this.leaders = data.leaderboard.sort(function(a,b) {return (parseInt(b.count) > parseInt(a.count)) ? 1 : ((parseInt(a.count) > parseInt(b.count)) ? -1 : 0);} );
+        this.leaders = data.leaderboard//.sort(function(a,b) {return (parseInt(b.count) > parseInt(a.count)) ? 1 : ((parseInt(a.count) > parseInt(b.count)) ? -1 : 0);} );
         console.log(this.leaders)
       })
     }
