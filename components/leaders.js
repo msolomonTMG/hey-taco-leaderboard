d642a5f121eb3214edd9c6287b6b57c3e1e7c89f
Mike Solomon <msolomon@thrillist.com>
2018-06-28 08:39:36 -0400

    transition tacos in

--- components/leaders.js
+++ components/leaders.js
@@ -16,9 +16,11 @@ Vue.component('leader-card', {
             <div class="col-10">
               <img :src=leader.avatar class="rounded-circle avatar float-left">
               <h4 class="card-title">{{leader.username}}</h4>
-              <span v-for="taco in tacos">
-                <img :src=tacoImageLocation height="18px" width="21px">
-              </span>
+              <transition-group name="taco-list" tag="span">
+                <span v-for="(taco, index) in tacos" v-bind:key="index" class="taco-list-item">
+                  <img :src=tacoImageLocation height="18px" width="21px">
+                </span>
+              </transition-group>
             </div>
             <div class="col-2">
               <h4 class="vertical-align float-right">
