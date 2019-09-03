88fd8dbeafb66f9b9a7a796640541bc7027d7c25
Mike Solomon <msolomon@thrillist.com>
2019-09-03 09:58:47 -0400

    no cors proxy

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var app = new Vue({
   methods: {
     loadLeaderboard: function() {
       console.log('fetching data')
-      fetch('https://cors-anywhere.herokuapp.com/https://www.heytaco.chat/api/v1.1/json/leaderboard/T376NB673?days=' + this.numDays)
+      fetch('https://www.heytaco.chat/api/v1.1/json/leaderboard/T376NB673?days=' + this.numDays)
       //fetch('https://heytacoleaderboard.firebaseio.com/.json')
       .then(response => response.json())
       .then(data => {
